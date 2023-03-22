cf78b57e68171e67fd1f63556ab7e5c510483645
LukasMandik <swipe47@gmail.com>
2023-03-22 12:42:13 +0100

    Changed django admin panel configuration and modified css gallery view.

--- lukas_mandik_photos/photos_web/admin.py
+++ lukas_mandik_photos/photos_web/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 
 from . import models
+from .models import Photo, Category
 
 
 
 
 
 # Register your models here.
-admin.site.register(models.Photo)
+# admin.site.register(models.Photo)
 admin.site.register(models.Category)
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['name', 'image_name', 'category', 'timestamp','png_image']
+    list_filter = []
+    list_editable = ['category']
+    # prepopulated_fields = {'slug': ('name',)}
