dac26845628a498c8dcfbafd4740d27efa9166e9
LukasMandik <swipe47@gmail.com>
2023-03-27 20:27:03 +0200

    Changed text animation in gallery items and changed navbar hiding

--- lukas_mandik_photos/static/js/script.js
+++ lukas_mandik_photos/static/js/script.js
@@ -34,18 +34,10 @@ function scrollHandler() {
 
   if (currentScrollPos > scrollPos && currentScrollPos > offset) { // ak sme posunuli pod určitý bod
     navBar.classList.add("small"); // pridajte triedu pre menší navigačný panel
-    // navBar.style.opacity = "0.5"; // nastavte opacity na 90%
-  } else { // ak sme posunuli nad určitý bod
-    navBar.classList.remove("small"); // odstráňte triedu pre menší navigačný panel
-    // navBar.style.opacity = "0.9"; // nastavte opacity na 100%
-  }
-
-  // ak sme posunuli smerom hore
-  // if (currentScrollPos < scrollPos) {
-  if (currentScrollPos > scrollPos && currentScrollPos > offset) {
     navBar.style.transition = "all 0.5s ease-in-out"; // pridajte transition
-  } else { // ak sme posunuli smerom dole
-    navBar.style.transition = "all 0.5s ease-in-out"; // odstráňte transition
+  } else if (currentScrollPos < (scrollPos - 5)) { // ak sme posunuli nad určitý bod
+    navBar.classList.remove("small"); // odstráňte triedu pre menší navigačný panel
+    navBar.style.transition = "all 0.5s ease-in-out"; // pridajte transition
   }
 
   scrollPos = currentScrollPos; // uložte novú pozíciu scrollovania
