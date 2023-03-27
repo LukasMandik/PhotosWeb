795175e4ad0d6d0b04ed81215b3b7589846e07f6
LukasMandik <swipe47@gmail.com>
2023-03-27 19:09:11 +0200

    Adjusted navbar transition hiding

--- lukas_mandik_photos/static/js/script.js
+++ lukas_mandik_photos/static/js/script.js
@@ -6,16 +6,47 @@ let menu = document.querySelector(".menu");
 	})
 
 
-var navBar = document.querySelector(".navbar"); // vyberte navigačný panel
-var offset = 100; // nastavte hodnotu posunu, po ktorom sa zmení veľkosť
-var lastScrollPos = 0;
+// var navBar = document.querySelector(".navbar"); // vyberte navigačný panel
+// var offset = 100; // nastavte hodnotu posunu, po ktorom sa zmení veľkosť
+// var lastScrollPos = 0;
+//
+// window.addEventListener("scroll", function() { // pridajte event listener na posun stránky
+//   var currentScrollPos = window.pageYOffset; // zistite aktuálnu pozíciu posunu stránky
+//   if (currentScrollPos > lastScrollPos && currentScrollPos > offset) { // ak sme posunuli smerom nadol a sme dostatočne ďaleko od začiatku stránky
+//     navBar.classList.add("small"); // pridajte triedu pre menší navigačný panel
+//   } else { // ak sme posunuli smerom nahor alebo sme príliš blízko začiatku stránky
+//     navBar.classList.remove("small"); // odstráňte triedu pre menší navigačný panel
+//   }
+//   lastScrollPos = currentScrollPos; // uložte aktuálnu pozíciu posunu ako poslednú
+// });
 
-window.addEventListener("scroll", function() { // pridajte event listener na posun stránky
-  var currentScrollPos = window.pageYOffset; // zistite aktuálnu pozíciu posunu stránky
-  if (currentScrollPos > lastScrollPos && currentScrollPos > offset) { // ak sme posunuli smerom nadol a sme dostatočne ďaleko od začiatku stránky
+
+
+var navBar = document.querySelector(".navbar"); // vyberte navigačný panel
+var offset = 70; // nastavte hodnotu posunu, po ktorom sa zmení veľkosť
+var scrollPos = 0; // uložte aktuálnu pozíciu scrollovania
+
+window.addEventListener("scroll", scrollHandler);
+window.addEventListener("load", scrollHandler);
+
+function scrollHandler() {
+  var currentScrollPos = window.pageYOffset;
+
+  if (currentScrollPos > scrollPos && currentScrollPos > offset) { // ak sme posunuli pod určitý bod
     navBar.classList.add("small"); // pridajte triedu pre menší navigačný panel
-  } else { // ak sme posunuli smerom nahor alebo sme príliš blízko začiatku stránky
+    // navBar.style.opacity = "0.5"; // nastavte opacity na 90%
+  } else { // ak sme posunuli nad určitý bod
     navBar.classList.remove("small"); // odstráňte triedu pre menší navigačný panel
+    // navBar.style.opacity = "0.9"; // nastavte opacity na 100%
   }
-  lastScrollPos = currentScrollPos; // uložte aktuálnu pozíciu posunu ako poslednú
-});
+
+  // ak sme posunuli smerom hore
+  // if (currentScrollPos < scrollPos) {
+  if (currentScrollPos > scrollPos && currentScrollPos > offset) {
+    navBar.style.transition = "all 0.5s ease-in-out"; // pridajte transition
+  } else { // ak sme posunuli smerom dole
+    navBar.style.transition = "all 0.5s ease-in-out"; // odstráňte transition
+  }
+
+  scrollPos = currentScrollPos; // uložte novú pozíciu scrollovania
+}
